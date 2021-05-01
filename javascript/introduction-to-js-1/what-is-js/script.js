0945ef4a16390db1e3bfdc31857843e8b66daf2c
chrystalme <afam_ifeanyi@live.com>
2021-05-01 21:47:01 +0300

    Error troubleshooting

--- javascript/introduction-to-js-1/what-is-js/script.js
+++ javascript/introduction-to-js-1/what-is-js/script.js
@@ -1,11 +1,20 @@
 function createParagraph() {
-  let para = document.createElement('p');
-  para.textContent = 'You clicked the button!';
+  let para = document.createElement("p");
+  para.textContent = "You clicked the button!";
   document.body.appendChild(para);
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
-for(let i = 0; i < buttons.length ; i++) {
-  buttons[i].addEventListener('click', createParagraph);
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", createParagraph);
+}
+
+function deleteParagraph() {
+  let para = document.getElementsByTagName("p");
+  para.textContent = "Welcome";
+}
+
+for (let i = 0; i < para.length; i++) {
+  para[i].addEventListener("click", deleteParagraph);
 }
